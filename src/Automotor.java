8319911a3dbc044ce74e389d4187f141a0071f08
David Garcia <davidsamuelgarciasanchez@gmail.com>
2020-12-29 13:40:51 -0300

    Se crea la primera clase hija de la clase abstracta Vehiculos

---
+++ src/Automotor.java
@@ -0,0 +1,18 @@
+public class Automotor extends Vehiculos {
+    private int cantPuertas;
+
+    public Automotor(String marca, String modelo, float precio, int cantPuertas) {
+        super(marca, modelo, precio);
+        this.cantPuertas = cantPuertas;
+    }
+
+    public int getCantPuertas() {
+        return cantPuertas;
+    }
+
+    @Override
+    public String caracteristicas() {
+        return "Marca: " + getMarca() + "// Modelo: " + getModelo() +
+                "// Puertas: " + getCantPuertas() + "// Precio: $" + getPrecioString() + "";
+    }
+}
